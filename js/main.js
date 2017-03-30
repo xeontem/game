861faa7542b15ccdcb5ef0b37e0ffb925a380f6d
xeontem <xeontem@gmail.com>
2017-03-30 23:54:22 +0300

    blabla

--- js/main.js
+++ js/main.js
@@ -13,6 +13,8 @@ let GameState = {
 		//this.mainPlayer.scale.setTo(-1, -1);
 		this.mainPlayer.scale.setTo(0.8);// scale
 		this.mainPlayer.angle = 90;
+		this.physics.enable(this.mainPlayer, Phaser.Physics.ARCADE);
+		this.cursors = this.input.keyboard.createCursorKeys();
 	},
 	update: function(){
 		// this.mainPlayer.angle += 1;
@@ -21,6 +23,22 @@ let GameState = {
 		this.mainPlayer.scale.setTo(1);
 
 		this.background.tilePosition.x -= 2;
+
+		this.mainPlayer.body.velocity.x = 0;
+		this.mainPlayer.body.velocity.y = 0;
+
+		if(this.cursors.left.isDown){
+			this.mainPlayer.body.velocity.x = -350;
+		}
+		if(this.cursors.right.isDown){
+			this.mainPlayer.body.velocity.x = 350;
+		}
+		if(this.cursors.up.isDown){
+			this.mainPlayer.body.velocity.y = -350;
+		}
+		if(this.cursors.down.isDown){
+			this.mainPlayer.body.velocity.y = 350;
+		}
 	}
 };
 game.state.add('GameState', GameState);
